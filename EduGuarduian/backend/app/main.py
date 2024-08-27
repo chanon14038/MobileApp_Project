39a5676fd9b7e29b84d8766407d90418c5ebf2b7
ชานนท์ จันโทนอ่อน <chanon.jant@obec.moe.go.th>
2024-08-27 16:47:55 +0700

    created flutter app to frontend

--- EduGuarduian/backend/app/main.py
+++ EduGuarduian/backend/app/main.py
@@ -2,6 +2,14 @@
 
 from fastapi import FastAPI
 
+
+engine = create_engine(
+    "postgresql+pg8000://postgres:123456@localhost/digimondb",
+    echo=True,
+    connect_args=connect_args,
+)
+
+
 app = FastAPI()
 
 @app.get("/")
