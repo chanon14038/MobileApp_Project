12308c9f3c2d59a5fb101889306d88c4eece5c82
chanon14038 <lnwchanon@gmail.com>
2024-09-25 00:27:49 +0700

    create profile_student page

---
+++ EduGuardian/frontend/app/lib/myclassroom/studentprofile_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../models/models.dart';
+
+class StudentProfilePage extends StatelessWidget {
+  final Student student;
+
+  const StudentProfilePage({required this.student});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Student profile"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Name: ${student.firstName} ${student.lastName}",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text("Student ID: ${student.studentId}",
+                style: const TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            Text("Classroom: ${student.classroom}",
+                style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            // Add more details if available in the Student model
+          ],
+        ),
+      ),
+    );
+  }
+}
