959327171b44b4e30dead68b3711a0bcf811b5ac
chanon14038 <lnwchanon@gmail.com>
2024-10-14 23:38:01 +0700

    fix bug db_classroom

--- EduGuardian/backend/app/routers/students.py
+++ EduGuardian/backend/app/routers/students.py
@@ -153,9 +153,11 @@ async def update_student(
     )
     db_advisor = result.one_or_none()
     
+    
     if db_student:
         db_student.sqlmodel_update(info)
         db_student.advisor = f"{db_advisor.first_name} {db_advisor.last_name}"
+        db_student.db_classroom = db_advisor.db_classroom
         session.add(db_student)
         await session.commit()
         await session.refresh(db_student)
