8b2075ce6350cf9d93f459ca4c9eb80558289af9
chanon14038 <lnwchanon@gmail.com>
2024-10-15 04:13:45 +0700

    ignore test db

--- EduGuardian/backend/app/models/__init__.py
+++ EduGuardian/backend/app/models/__init__.py
@@ -43,7 +43,7 @@ async def create_table():
 
 async def recreate_table():
     async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.drop_all)
+        # await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
